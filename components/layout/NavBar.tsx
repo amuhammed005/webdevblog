cda10be1a2bf201146fec2771daf92d6743f2fb9
amuhammed005 <amuhammed005@st.ug.edu.gh>
2026-03-10 04:06:41 +0000

    Added links in nav to login and register routes in auth

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -5,6 +5,7 @@ import { MdEditNote } from "react-icons/md";
 import SearchInput from "./SearchInput";
 import Notifications from "./Notifications";
 import UserButton from "./UserButton";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
@@ -20,6 +21,10 @@ const NavBar = () => {
             <ThemeToggle />
             <Notifications />
             <UserButton />
+            <>
+              <Link href="/login">Login</Link>
+              <Link href="/register">Register</Link>
+            </>
           </div>
         </div>
       </Container>
