49264fd5078a2d7355ed7d048f5793075e57d7cf
amuhammed005 <amuhammed005@st.ug.edu.gh>
2026-03-10 02:33:06 +0000

    feat: auth page, feat: dynamic user  page

--- components/layout/UserButton.tsx
+++ components/layout/UserButton.tsx
@@ -1,23 +1,61 @@
 import React from "react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
-import { FaRegUser } from "react-icons/fa";
+import { LogOut, Pencil, Shield, User, UserRound } from "lucide-react";
+import { FaRegBookmark } from "react-icons/fa";
 
 const UserButton = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="User Icon" />
-          <AvatarFallback>
-            <FaRegUser size={18} />
+          <AvatarImage src="" alt="User Icon" />
+          <AvatarFallback className="border-2 border-slate-500 dark:border-slate-50">
+            <UserRound />
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent >
+      <DropdownMenuContent>
         <DropdownMenuItem>
-            <button className="w-full text-left">Sign out</button>
-            Profile</DropdownMenuItem>
+          <button className="w-full text-left flex items-center gap-4">
+            <User size={18} />
+            Profile
+          </button>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <button className="w-full text-left flex items-center gap-4">
+            <Pencil size={16} />
+            Create Post
+          </button>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <button className="w-full text-left flex items-center gap-4">
+            <FaRegBookmark size={16} />
+            Bookmark
+          </button>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <button className="w-full text-left flex items-center gap-4">
+            <Shield size={18} />
+            Admin
+          </button>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <button className="w-full text-left flex items-center gap-4">
+            <LogOut size={18} />
+            Sign Out
+          </button>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
