4d2436c4fbcc9dd11cd8992e0cf2bea23d4c4b56
amuhammed005 <amuhammed005@st.ug.edu.gh>
2026-03-18 06:15:34 +0000

    fix: chnage toggle fxn in themtoggle, refactor register.ts flie

--- actions/auth/register.ts
+++ actions/auth/register.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { RegisterSchema, RegisterSchemaType } from "@/schemas/RegisterSchema";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 export const SignUp = async (values: RegisterSchemaType) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -12,9 +12,13 @@ export const SignUp = async (values: RegisterSchemaType) => {
 
   const {name, email, password} = validateFields.data;
 
-  const hashedPassword = await bcryptjs.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
+  
+  // Here you can add your logic to create a new user in the database
+  // For example, you can use an ORM like Prisma or Mongoose to interact with your database
 
+  // Example using Prisma:
   await prisma.user.create({
     data: {
       name,
@@ -22,18 +26,6 @@ export const SignUp = async (values: RegisterSchemaType) => {
       password: hashedPassword, // Store the hashed password in the database
     }
   })
-
-  // Here you can add your logic to create a new user in the database
-  // For example, you can use an ORM like Prisma or Mongoose to interact with your database
-
-  // Example using Prisma:
-  // await prisma.user.create({
-  //   data: {
-  //     name,
-  //     email,
-  //     password, // Make sure to hash the password before storing it
-  //   },
-  // });
 
   return { success: true };
 };
--- components/layout/ThemeToggle.tsx
+++ components/layout/ThemeToggle.tsx
@@ -6,17 +6,17 @@ import { useTheme } from 'next-themes'
 const ThemeToggle = () => {
   const {theme, setTheme} = useTheme()
 
-  const toggleTheme = () => {
+  const handleToggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   }
 
   console.log("theme>>", theme)
   return (
-    <button onClick={toggleTheme} >
-        <Sun className='hidden dark:block'/>
-        <Moon className='block dark:hidden' />
+    <button onClick={handleToggleTheme}>
+      <Sun className="hidden dark:block" />
+      <Moon className="block dark:hidden" />
     </button>
-  )
+  );
 }
 
 export default ThemeToggle
